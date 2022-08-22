9b59afbe60c2d7e52781645057e88a594d921d4d
MirThomasson <mirthomasson@gmail.com>
2022-08-22 10:35:22 -0400

    first commit

---
+++ src/components/Hero.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './Hero.css';
+
+function Hero() {
+  return (
+    <div className='hero-container'>
+      <video src="./hero-vid.mp4" autoPlay loop muted />
+      <h1>Demolition & Excavation Company</h1>
+      <p>Serving Amherst, Va and surrouding areas</p>
+      <div className="hero-btns">
+        <Button className='btns' buttonStyle='btn--outline'
+        buttonSize='btn--large'>
+            Start Here
+        </Button>
+        <Button className='btns' buttonStyle='btn--primary'
+        buttonSize='btn--large'>
+            Watch Video <i className='far fa-play-circle' />
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/components/Navbar.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from './Button';
+import './Navbar.css';
+import Logo from './logo.png';
+
+
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButtton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButtton();
+  }, []);
+
+  window.addEventListener('resize', showButtton);
+
+  return (
+    <>
+    <nav className='navbar'>
+       <div className='navbar-container'>
+        <Link to='/' className="navbar-logo" onClick={closeMobileMenu}>
+          <img src={Logo} alt='logo' id='logo'/>
+            DIRT WORKS <i className="fab.fa-typo3" />    
+        </Link>
+        <div className='menu-icon' onClick={handleClick}>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+        </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li className='nav-item'>
+            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+              HOME
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+              SERVICES
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/contact' className='nav-links' onClick={closeMobileMenu}>
+              CONTACT
+            </Link> 
+          </li>
+          <li>
+              <Link
+                to='/sign-up'
+                className='nav-links-mobile'
+                onClick={closeMobileMenu}
+              >
+                Free Estimate
+              </Link>
+            </li>
+        </ul>
+        {button && <Button buttonStyle='btn--outline'>Free Estimate</Button>}
+       </div>
+       </nav>
+    </>
+  )
+}
+
+export default Navbar
